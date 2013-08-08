81c63334000a2452bad50389ad0beb06bdcc968f
Kevin Schaul <kevin.schaul@gmail.com>
2013-08-08 18:01:30 -0400

    Added test project structure

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var depict = require('../');
+
+describe('utility', function() {
+  describe('#cleanURL()', function() {
+    it('should prepent `http://` if no protocol is specified', function() {
+      assert.equal(0, 0);
+    });
+  });
+});
+
