d49a3297ffb47b7317b069279f7c4476ccf51257
mhkeller <michael.keller@gmail.com>
2013-10-25 17:17:57 -0400

    functional as function

--- src/depict.js
+++ src/depict.js
@@ -1,80 +1,36 @@
 #!/usr/bin/env node
 
 var child_process = require('child_process');
-var fs = require('fs');
-var optimist = require('optimist');
-var phantom = require('phantom');
-var sleep   = require('sleep');
+var fs            = require('fs');
+var _             = require('underscore')
+var phantom       = require('phantom');
+var sleep         = require('sleep');
 
-var argv = optimist
-  .usage('Usage: depict URL OUT_FILE [OPTIONS]')
-  .options('h', {
-    alias: 'help',
-    describe: 'Display help',
-    default: false
-  })
-  .options('s', {
-    alias: 'selector',
-    describe: 'CSS selector',
-    default: 'body'
-  })
-  .options('c', {
-    alias: 'css',
-    describe: 'CSS file to include in rendering',
-    default: false
-  })
-  .options('H', {
-    alias: 'hide-selector',
-    describe: 'Hide attributes of this selector berore rendering.',
-    default: false
-  })
-  .options('w', {
-    alias: 'browser-width',
-    describe: 'Specify the desired browser width.',
-    default: 1440
-  })
-  .options('d', {
-    alias: 'delay',
-    describe: 'Specify a delay time, in seconds.',
-    default: 5
-  })
-  .check(function(argv) {
-    if (argv._.length !== 2) throw new Error('URL and OUT_FILE must be given.');
-  })
-  .argv;
-
-if (argv.h || argv.help) return optimist.showHelp();
-
-// Append 'http://' if protocol not specified
-var url = argv._[0];
-if (!url.match(/^\w+:\/\//)) {
-  url = 'http://' + url;
+var settings = {
+  mode: 'save',
+  viewport_width: 1440,
+  delay: 5,
+  selector: 'body'
 }
 
-var selector = argv.s || argv.selector;
-var out_file = argv._[1];
+function depict(opts) {
+  _.extend(settings, opts);
 
-var css_file = argv.c || argv.css;
-var css_text = '';
-if (css_file) {
-    css_text = fs.readFileSync(css_file, 'utf8');
-}
+  // Append 'http://' if protocol not specified
+  if (!settings.url.match(/^\w+:\/\//)) {
+    settings.url = 'http://' + settings.url;
+  }
 
-var hide_selector = argv.H || argv["hide-selector"];
-if (hide_selector) {
-  css_text += "\n\n " + hide_selector + " { display: none; }\n";
-}
+  var css_text;
+  if (settings.css_hide){
+    css_text = settings.css_file += "\n\n " + settings.css_hide + " { display: none; }\n";
+  }
 
-var viewport_width = argv.w || argv['browser-width'];
-var delay_time     = argv.d || argv['delay'];
-
-function depict(url, out_file, selector, css_text) {
   // phantomjs heavily relies on callback functions
-
   var page;
   var ph;
 
-  console.log('\nRequesting', url);
+  console.log('\nRequesting', settings.url);
 
   phantom.create(createPage)
 
@@ -86,12 +42,12 @@ function depict(url, out_file, selector, css_text) {
   function openPage(_page) {
     page = _page;
     page.set('onError', function() { return; });
-    page.open(url, prepForRender);
-    page.set('viewportSize', {width: viewport_width, height: 900});
+    page.set('viewportSize', {width: settings.viewport_width, height: 900});
+    page.open(settings.url, prepForRender);
   }
 
   function prepForRender(status) {
-    page.evaluate(runInPhantomBrowser, renderImage, selector, css_text);
+    page.evaluate(runInPhantomBrowser, renderImage, settings.selector, css_text);
   }
 
   function runInPhantomBrowser(selector, css_text) {
@@ -106,16 +62,27 @@ function depict(url, out_file, selector, css_text) {
   }
 
   function renderImage(rect) {
-    sleep.sleep(delay_time);
+    sleep.sleep(settings.delay);
     page.set('clipRect', rect);
-    page.render(out_file, cleanup);
+    if (settings.mode != 'save'){
+      console.log(page.renderBase64('PNG'));
+      cleanup();
+    }else{
+      page.render(settings.out_file, cleanup)
+    }
+    // page.render(out_file, cleanup);
   }
 
   function cleanup() {
-    console.log('Saved imaged to', out_file);
+    // console.log(this)
+    console.log('Saved imaged to', settings.out_file);
     ph.exit();
   }
 }
 
-depict(url, out_file, selector, css_text);
+module.exports = {
+  capture: depict
+}
+
+// depict(opts);
 
